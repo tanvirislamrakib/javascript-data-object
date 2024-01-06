355974ea6bd60cbe9787875652e3d2bd61878b60
tanvirislamrakib <tanvirislamrakib93@gmail.com>
2024-01-06 18:43:29 +0600

    This is javascript date code

---
+++ date.js
@@ -0,0 +1,11 @@
+var myDate = new Date();
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.getMonth());
+console.log(myDate.getDate());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
+console.log(myDate.getMilliseconds());
+//console.log(myDate.getNenoSecounds());
+console.log(myDate.getHours());
